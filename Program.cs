135397675155d21a015a66d4d1a617f020998f8f
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-12-09 13:35:10 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AulaPOO_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
